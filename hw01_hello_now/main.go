0e9f5e02a5061fe8eeee392a3e58a5e38f291deb
Zakharov P.B <p.b.zakharov@gmail.com>
2020-03-03 22:50:24 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const urlNtp = "0.beevik-ntp.pool.ntp.org"
+
 func main() {
-	// Place your code here
+	currentTime := time.Now()
+	exactTime, err := ntp.Time(urlNtp)
+	if err != nil {
+		log.Fatalf("ERROR get time from %s: %s", urlNtp, err)
+	}
+	fmt.Printf("current time: %v\n", currentTime)
+	fmt.Printf("exact time: %v\n", exactTime)
 }
