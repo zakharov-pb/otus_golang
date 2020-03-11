637a68323e57a49bce3b97964ae2c3b751471675
p.b.zakharov <p.b.zakharov@gmail.com>
2020-03-11 15:21:23 +0300

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
+// ErrInvalidString error returned from function Unpack
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+// Unpack function to unzip an archived string
+func Unpack(archive string) (string, error) {
+	var result strings.Builder
+	var prevSymb rune = -1
+	shielding := false
+	for _, r := range archive {
+		if unicode.IsDigit(r) && !shielding {
+			if prevSymb < 0 {
+				return "", ErrInvalidString
+			}
+			count := int(r - '0')
+			for i := 0; i < count; i++ {
+				result.WriteRune(prevSymb)
+			}
+			prevSymb = -1
+		} else {
+			if shielding && !unicode.IsDigit(r) && r != '\\' {
+				return "", ErrInvalidString
+			}
+			if !shielding && r == '\\' {
+				shielding = true
+				continue
+			}
+
+			if prevSymb > 0 {
+				result.WriteRune(prevSymb)
+			}
+			prevSymb = r
+			shielding = false
+		}
+	}
+	if prevSymb > 0 {
+		result.WriteRune(prevSymb)
+	}
+	return result.String(), nil
 }
