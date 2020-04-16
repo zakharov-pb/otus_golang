bf29dbacb8c2b15d7cb686c5f82672d4146d4e16
Zakharov P.B <p.b.zakharov@gmail.com>
2020-04-16 18:34:06 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,108 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
 )
 
+// ErrErrorsLimitExceeded result Run
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
+// Task interface for Run
 type Task func() error
 
+// Running N gorutin. The function returns a channel signaling the completion of all goroutines
+func runGorutins(count int, taskCh <-chan Task, errorCh chan<- error) <-chan struct{} {
+	result := make(chan struct{})
+	go func() {
+		var waiting sync.WaitGroup
+		waiting.Add(count)
+		for i := 0; i < count; i++ {
+			// Run gorutin
+			go func() {
+				defer waiting.Done()
+				for t := range taskCh {
+					errorCh <- t()
+				}
+			}()
+		}
+		waiting.Wait()
+		// All goroutines are completed
+		close(result)
+	}()
+	return result
+}
+
+var errorCounter = func(maxCountErrors int) func(error) bool {
+	errorCount := 0
+	return func(e error) bool {
+		if maxCountErrors < 0 {
+			return true
+		}
+		if errorCount >= maxCountErrors {
+			return false
+		}
+		if e != nil {
+			errorCount++
+		}
+		if errorCount >= maxCountErrors {
+			return false
+		}
+		return true
+	}
+}
+
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
-func Run(tasks []Task, N int, M int) error {
-	// Place your code here
-	return nil
+func Run(tasks []Task, n int, m int) (errorResult error) {
+	errorCh := make(chan error)
+	taskCh := make(chan Task)
+
+	safeCloseTasks := func() func() {
+		isClosed := false
+		return func() {
+			if isClosed {
+				return
+			}
+			close(taskCh)
+			isClosed = true
+		}
+	}()
+	waitingCh := runGorutins(n, taskCh, errorCh)
+	checkError := errorCounter(m)
+
+	indexTask := 0
+	countTasks := len(tasks)
+	// error handling. Close channel when error limit is reached
+	handlerError := func(e error) {
+		if !checkError(e) {
+			if errorResult == nil {
+				errorResult = ErrErrorsLimitExceeded
+				indexTask = countTasks
+				safeCloseTasks()
+			}
+		}
+	}
+	for {
+		if indexTask < countTasks {
+			// Send tasks and process errors
+			select {
+			case taskCh <- tasks[indexTask]:
+				indexTask++
+				if indexTask >= countTasks {
+					safeCloseTasks()
+				}
+			case e := <-errorCh:
+				handlerError(e)
+			case <-waitingCh:
+				return errorResult
+			}
+			continue
+		}
+		// Process errors from goroutines
+		select {
+		case e := <-errorCh:
+			handlerError(e)
+		case <-waitingCh:
+			return errorResult
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -30,8 +30,7 @@ func TestRun(t *testing.T) {
 		result := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Equal(t, ErrErrorsLimitExceeded, result)
-		require.LessOrEqual(t,
-			int32(workersCount+maxErrorsCount), runTasksCount, "extra tasks were started")
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
@@ -60,7 +59,76 @@ func TestRun(t *testing.T) {
 		elapsedTime := time.Since(start)
 		require.Nil(t, result)
 
-		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+}
+
+func TestAdditionalRun(t *testing.T) {
+	t.Run("Number of tasks is less than goroutin and ignore errors", func(t *testing.T) {
+		tasksCount := 2
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			e := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return e
+			})
+		}
+
+		workersCount := 4
+		maxErrorsCount := -1
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, nil, result)
+	})
+
+	t.Run("Number of tasks is less than goroutin and without ignoring errors", func(t *testing.T) {
+		tasksCount := 2
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			e := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return e
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, ErrErrorsLimitExceeded, result)
+	})
+
+	t.Run("A large number of tasks", func(t *testing.T) {
+		tasksCount := 100000
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			e := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return e
+			})
+		}
+
+		workersCount := 500
+		maxErrorsCount := -1
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, nil, result)
+	})
+
 }
