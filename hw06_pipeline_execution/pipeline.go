258de64c6638a631e9ee9ab7af1433ebbabe928a
Zakharov P.B <p.b.zakharov@gmail.com>
2020-04-17 23:51:48 +0300

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,15 +1,47 @@
 package hw06_pipeline_execution //nolint:golint,stylecheck
 
 type (
-	I   = interface{}
-	In  = <-chan I
+	// I define interface
+	I = interface{}
+	// In channel of read data
+	In = <-chan I
+	// Out channel of read data
 	Out = In
-	Bi  = chan I
+	// Bi input-output channel
+	Bi = chan I
 )
 
+// Stage worker
 type Stage func(in In) (out Out)
 
+// ExecutePipeline function create pipeline
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	countStages := len(stages)
+	if countStages == 0 || in == nil {
+		return nil
+	}
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- val:
+				case <-done:
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	o := stages[0](out)
+	if countStages > 1 { //nolint:gomnd
+		return ExecutePipeline(o, done, stages[1:]...)
+	}
+	return o
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -1,7 +1,11 @@
 package hw06_pipeline_execution //nolint:golint,stylecheck
 
 import (
+	"encoding/json"
+	"fmt"
+	"regexp"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -12,6 +16,30 @@ const (
 	sleepPerStage = time.Millisecond * 100
 	fault         = sleepPerStage / 2
 )
+
+type Tester struct {
+	ID      int       `json:"id"`
+	Name    string    `json:"name"`
+	Info    string    `json:"info"`
+	Created time.Time `json:"created"`
+}
+
+func CreateTester(id int, name string, info string) *Tester {
+	return &Tester{
+		ID:      id,
+		Name:    name,
+		Info:    info,
+		Created: time.Date(2020, time.April, 1, 0, 0, 0, 0, time.Local),
+	}
+}
+
+func (t *Tester) String() string {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return fmt.Sprintf(`{"error": "%v"}`, err)
+	}
+	return string(data)
+}
 
 func TestPipeline(t *testing.T) {
 	// Stage generator
@@ -69,7 +97,8 @@ func TestPipeline(t *testing.T) {
 		// Abort after 200ms
 		abortDur := sleepPerStage * 2
 		go func() {
-			done <- <-time.After(abortDur)
+			<-time.After(abortDur)
+			close(done)
 		}()
 
 		go func() {
@@ -88,5 +117,109 @@ func TestPipeline(t *testing.T) {
 
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
+	})
+}
+
+func TestAdditionalPipeline(t *testing.T) {
+	// Unformatted information
+	data := []interface{}{
+		0.5,
+		`Hello
+							
+			World!`,
+		1000,
+		CreateTester(
+			1,
+			"User     01",
+			`Unformatted     TeXt`,
+		),
+	}
+	reference := []string{"0.5", "HELLO WORLD!", "1000", "{'ID':1,'NAME':'USER 01','INFO':'UNFORMATTED TEXT','CREATED':'2020-04-01T00:00:00+03:00'}"}
+	// String stage generator
+	g := func(name string, f func(v string) string) Stage {
+		return func(in In) Out {
+			out := make(Bi)
+			go func() {
+				defer close(out)
+				for v := range in {
+					time.Sleep(sleepPerStage)
+					str := fmt.Sprintf("%v", v)
+					out <- f(str)
+				}
+			}()
+			return out
+		}
+	}
+	stages := []Stage{
+		g("Trim space", func() func(string) string {
+			var re = regexp.MustCompile(`[ \t\n]+`)
+			return func(v string) string { return re.ReplaceAllString(v, " ") }
+		}()),
+		g("Replace", func(v string) string { return strings.ReplaceAll(v, "\"", "'") }),
+		g("ToUpper", func(v string) string { return strings.ToUpper(v) }),
+	}
+
+	t.Run("string case", func(t *testing.T) {
+		in := make(Bi)
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, result, reference)
+		require.Less(t,
+			int64(elapsed),
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault),
+		)
+	})
+
+	t.Run("stages nill case", func(t *testing.T) {
+		start := time.Now()
+		require.Nil(t, ExecutePipeline(nil, nil))
+		elapsed := time.Since(start)
+		require.Less(t,
+			int64(elapsed),
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
+	})
+
+	t.Run("input nill case", func(t *testing.T) {
+		result := make([]interface{}, 0)
+		start := time.Now()
+		require.Nil(t, ExecutePipeline(nil, nil, stages...))
+		elapsed := time.Since(start)
+
+		require.Equal(t, len(result), 0)
+		require.Less(t,
+			int64(elapsed),
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
+	})
+
+	t.Run("call done", func(t *testing.T) {
+		done := make(Bi)
+		in := make(Bi)
+		go func() {
+			defer close(done)
+			for index, v := range data {
+				if index == 2 {
+					break
+				}
+				in <- v
+			}
+			time.Sleep(time.Millisecond * 10)
+		}()
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		require.Less(t, len(result), len(reference))
 	})
 }
