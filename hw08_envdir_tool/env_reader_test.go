cdf763931c1e806716f9e98b8569e46fa86be7e3
Zakharov P.B <p.b.zakharov@gmail.com>
2020-05-31 15:05:20 +0300

    hw08_envdir_tool

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("default tests", func(t *testing.T) {
+		path := "./testdata/env"
+		ioutil.WriteFile(path+"/UNSET1", []byte("ERROR"), 0644)
+		defer os.Remove(path + "/UNSET1")
+
+		info, err := ReadDir(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(info) != 5 {
+			t.Fatal("error count")
+		}
+		if v, ok := info["BAR"]; !ok || v != "bar" {
+			t.Fatal("error get value BAR")
+		}
+		if v, ok := info["FOO"]; !ok || v != "   foo\nwith new line" {
+			t.Fatal("error get value FOO")
+		}
+		if v, ok := info["UNSET"]; !ok || len(v) > 0 {
+			t.Fatal("error get value UNSET")
+		}
+		if v, ok := info["HELLO"]; !ok || v != `"hello"` {
+			t.Fatal("error get value UNSET")
+		}
+		if v, ok := info["UNSET1"]; !ok || v != `ERROR` {
+			t.Fatal("error get value UNSET1")
+		}
+	})
 }
